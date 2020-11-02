58084a7179a5a517caffd0ab0c1f111c841c07a3
Melissa F <melissa.faulner@gmail.com>
2020-11-02 02:47:59 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,8 +2,8 @@ def square_array(array)
   new_array = [ ]
   counter = 0
   while counter < array.length do
-    new_array[counter] = array[counter]**2
-  counter =+ 1
+  new_array[counter] = array[counter]**2
+  counter += 1
   end
   new_array
 end
